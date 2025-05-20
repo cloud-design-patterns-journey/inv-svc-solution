492a938c627860884bafbaf0165f1056c1af2b7a
Noé Samaille <noe.samaille@ibm.com>
2025-05-20 17:06:35 +0200

    Added CRUD operations
    Signed-off-by: Noé Samaille <noe.samaille@ibm.com>

--- src/main/java/com/ibm/inventory_management/controllers/StockItemController.java
+++ src/main/java/com/ibm/inventory_management/controllers/StockItemController.java
@@ -2,8 +2,7 @@ package com.ibm.inventory_management.controllers;
 
 import java.util.List;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.ibm.inventory_management.models.StockItem;
 import com.ibm.inventory_management.services.StockItemApi;
@@ -11,14 +10,31 @@ import com.ibm.inventory_management.services.StockItemApi;
 @RestController
 public class StockItemController {
 
-  private final StockItemApi service;
+    private final StockItemApi service;
 
-  public StockItemController(StockItemApi service) {
-    this.service = service;
-  }
+    public StockItemController(StockItemApi service) {
+        this.service = service;
+    }
 
     @GetMapping(path = "/stock-items", produces = "application/json")
-  public List<StockItem> listStockItems() {
-    return this.service.listStockItems();
-  }
+    public List<StockItem> listStockItems() {
+        return this.service.listStockItems();
+    }
+
+    @PostMapping(path = "/stock-item")
+    public void addStockItem(@RequestParam String name, @RequestParam String manufacturer, @RequestParam float price,
+            @RequestParam int stock) {
+        this.service.addStockItem(name, manufacturer, price, stock);
+    }
+
+    @PutMapping(path = "/stock-item/{id}")
+    public void updateStockItem(@PathVariable("id") String id, @RequestParam String name,
+            @RequestParam String manufacturer, @RequestParam float price, @RequestParam int stock) {
+        this.service.updateStockItem(id, name, manufacturer, price, stock);
+    }
+
+    @DeleteMapping(path = "/stock-item/{id}")
+    public void deleteStockItem(@PathVariable("id") String id) {
+        this.service.deleteStockItem(id);
+    }
 }
